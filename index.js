8b6bfc52a0de4063dca92c10417c9bbfb5922e65
Badm1 <shattilov@mail.ru>
2021-04-11 13:46:06 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let btn = document.querySelector('button')
+
+
+btn.onclick=function(){
+    btn.classList.toggle('active')
+}
+
+// function btnClick() {
+//     let btn = document.querySelector('button')
+//     btn.classList.toggle('active')
+// }
